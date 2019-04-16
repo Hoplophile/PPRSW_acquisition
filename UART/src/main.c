6bdfc05f3e2dba73523a764e620e86a5b7d29dd6
Piotr Ziebinski <piotr.ziebinski97@gmail.com>
2019-04-16 19:13:48 +0200

    UART\1

---
+++ UART/src/main.c
@@ -0,0 +1,24 @@
+#include <asf.h>
+
+#define UART_TXPIN IOPORT_CREATE_PIN(PORTE, 3)
+
+int main (void) {
+	
+	static usart_rs232_options_t USART_SERIAL_OPTIONS = {
+		.baudrate = 9600,
+		.charlength = USART_CHSIZE_8BIT_gc,
+		.paritytype = USART_PMODE_DISABLED_gc,
+		.stopbits = false
+	};
+	
+	stdio_serial_init(&USARTE0, &USART_SERIAL_OPTIONS);
+	ioport_set_pin_dir(UART_TXPIN, IOPORT_DIR_OUTPUT);
+	
+	while(1) {
+		
+		char ch;
+		
+		scanf("%c",&ch);
+		printf("%c",ch);
+	}
+}
