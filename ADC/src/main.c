72631f930d2aba7f57366fe3833c4cb0372f1f2f
Piotr Ziebinski <piotr.ziebinski97@gmail.com>
2019-04-20 09:50:02 +0200

    DIFF\2

--- ADC/src/main.c
+++ ADC/src/main.c
@@ -10,10 +10,10 @@ static void adc_init(void) {
 	struct adc_channel_config adcch_conf;
 	adc_read_configuration(&MY_ADC, &adc_conf);
 	adcch_read_configuration(&MY_ADC, MY_ADC_CH, &adcch_conf);
-	adc_set_conversion_parameters(&adc_conf, ADC_SIGN_OFF, ADC_RES_12, ADC_REF_BANDGAP);
+	adc_set_conversion_parameters(&adc_conf, ADC_SIGN_ON, ADC_RES_12, ADC_REF_BANDGAP);
 	adc_set_conversion_trigger(&adc_conf, ADC_TRIG_MANUAL, 1, 0);
 	adc_set_clock_rate(&adc_conf, 200000UL);
-	adcch_set_input(&adcch_conf, ADCCH_POS_PIN1, ADCCH_NEG_NONE, 1);
+	adcch_set_input(&adcch_conf, ADCCH_POS_PIN1, ADCCH_NEG_PIN5, 4);
 	adc_write_configuration(&MY_ADC, &adc_conf);
 	adcch_write_configuration(&MY_ADC, MY_ADC_CH, &adcch_conf);
 }
@@ -42,12 +42,12 @@ int main (void) {
 		
 		scanf("%c", &buffer);
 		if(buffer){
-			for(int sampleCounter = 0; sampleCounter < 16; sampleCounter++){
+			for(int sampleCounter = 0; sampleCounter < 1; sampleCounter++){
 				adc_start_conversion(&MY_ADC, MY_ADC_CH);
 				adc_wait_for_interrupt_flag(&MY_ADC, MY_ADC_CH);
-				result += adc_get_result(&MY_ADC, MY_ADC_CH);
+				result += adc_get_signed_result(&MY_ADC, MY_ADC_CH);
 			}
-			result /= 16;
+			result /= 1;
 			printf("%d\n", result);
 		}
 		
